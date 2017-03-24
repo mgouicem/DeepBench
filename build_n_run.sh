9cf2e33d639bb04e18083482c83de353dd763659
Mourad Gouicem <mourad.gouicem@intel.com>
2017-03-24 11:37:30 -0700

    improving the launch script

--- build_n_run.sh
+++ build_n_run.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
 workdir=/project/mgouicem/MKL-DNN/DeepBench/
-nb_cores=${nb_cores:-$(lscpu | awk '/Socket/{ print $2*nc } /Core/{ nc=$4} ')}
+
+
+#takes the parameters from the command line
+eval "$@"
+
+
 input_h=${input_h:-input_topologies.h}
+# detects the number of physical cores
+nb_cores=${nb_cores:-$(lscpu | awk '/Socket/{ print $2*nc } /Core/{ nc=$4} ')}
+#detects the flat mode but looking for a node without cores
+pre_cmd=$(numactl --hardware | awk '/node [0-9] cpus:$/ {print "numactl -m "$2; quit}')
+#selects which test to run
+run=${run:-mkl,mkldnn,libxsmm}
 
+tools_dir=/nfs/site/proj/mkl/mirror/NS/MKLQA/tools/setenv
+export MKLDNNROOT=/project/mgouicem/MKL-DNN/ipl_mkl_dnn-master/install/
+export LIBXSMMROOT=/project/mgouicem/MKL-DNN/libxsmm
 
-source /nfs/site/proj/mkl/mirror/NS/MKLQA/tools/setenv/setup_lnx_comp.sh 32e gnu491
-source /nfs/site/proj/mkl/mirror/NS/MKLQA/tools/setenv/setup_lnx_comp.sh 32e 170
-source /nfs/site/proj/mkl/mirror/NS/MKLQA/tools/setenv/setup_lnx_mpi.sh 32e 2017
-
+source $tools_dir/setup_lnx_comp.sh 32e gnu491
+source $tools_dir/setup_lnx_comp.sh 32e 170
+source $tools_dir/setup_lnx_mpi.sh 32e 2017
 source ~/setup_mkl_nightly.sh mkl2017u3 latest
+
 
 if [[ $(lscpu | awk '/Thread/ {print $4}') > 1 ]]
 then
@@ -17,11 +31,6 @@ then
 else
     export KMP_AFFINITY=granularity=fine,compact 
 fi
-
-MKLDNNROOT=/project/mgouicem/MKL-DNN/ipl_mkl_dnn-master/install/
-export LD_LIBRARY_PATH=$MKLDNNROOT/lib:$LD_LIBRARY_PATH
-
-export LIBXSMMROOT=/project/mgouicem/MKL-DNN/libxsmm
 
 function run_bench_mkl {
     local library_tested=$1
@@ -36,7 +45,7 @@ $library_tested  Convolution - ${num_threads} threads
 started...
 EOF
 
-        ./std_conv_bench 0 --csv-output >> $workdir/logs/$output_file
+        $pre_cmd ./std_conv_bench 0 --csv-output >> $workdir/logs/$output_file
         
         cat <<EOF
 done
@@ -50,13 +59,22 @@ timestamp=$(date +%Y%m%d_%H%M%S)
 cd $workdir/code/intel/convolution/mkl_conv
 
 ## Pure MKL bench
-make -B clean all INPUT_H=$input_h CONVLIB=MKL
-run_bench_mkl "MKL" "${timestamp}-mkl.csv"
+if [[ grep -wq mkl <<< $run ]]
+then
+    make -B clean all INPUT_H=$input_h CONVLIB=MKL
+    run_bench_mkl "MKL" "${timestamp}-mkl.csv"
+fi
 
 ## MKL-DNN bench
-make -B clean all INPUT_H=$input_h CONVLIB=MKLDNN
-run_bench_mkl "MKL-DNN" "${timestamp}-mkldnn.csv"
+if [[ grep -wq mkldnn <<< $run ]]
+then
+    make -B clean all INPUT_H=$input_h CONVLIB=MKLDNN MKLDNNROOT=$MKLDNNROOT
+    run_bench_mkl "MKL-DNN" "${timestamp}-mkldnn.csv"
+fi
 
 # libxsmm bench
-make -B clean all INPUT_H=$input_h CONVLIB=LIBXSMM LIBXSMMROOT=$LIBXSMMROOT
-run_bench_mkl "LIBXSMM" "${timestamp}-libxsmm.csv"
+if [[ grep -wq libxsmm <<< $run ]]
+then
+    make -B clean all INPUT_H=$input_h CONVLIB=LIBXSMM LIBXSMMROOT=$LIBXSMMROOT
+    run_bench_mkl "LIBXSMM" "${timestamp}-libxsmm.csv"
+fi
